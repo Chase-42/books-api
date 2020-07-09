5391836318ea00be46abe1fa54265645bebf978a
Chase-42 <chaseofthecollins@gmail.com>
2020-07-09 14:49:21 -0600

    User auth and .env

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import auth from './routes/auth';
 
+dotenv.config();
 const app = express();
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({
-    errors: { global: 'Invalid credentials' },
-  });
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
+
+app.use(bodyParser.json());
+app.use('/api/auth', auth);
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
